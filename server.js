8ea710dcfca4817b7c07cb811fddfa12531f9394
Colin Deeb <colindeeb@gmail.com>
2012-02-22 23:00:52 -0800

    added /assets request and response

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 var webServer = require('express').createServer();
 webServer.listen(3005);
+
+webServer.get('/assets/*', function(request, response){
+    //forward assests requests to site/assets folder
+    //request.params[0] is the requested filename
+    response.sendfile(__dirname + '/site/assets/' + request.params[0]);
+});
+
 webServer.get('/', function(request, response){
     response.sendfile(__dirname + '/site/index.html');
 });
