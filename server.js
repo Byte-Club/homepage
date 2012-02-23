c0f9b54f23683c6b589ea373081f7273d92318b5
Chip Deubner <cdeubner@Chip-T420s.(null)>
2012-02-22 22:26:18 -0800

    changed server.js

--- server.js
+++ server.js
@@ -1,2 +1,5 @@
 var webServer = require('express').createServer();
 webServer.listen(3005);
+webServer.get('/', function(request, response){
+    response.sendfile(__dirname + '/site/index.html');
+});
