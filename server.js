852761388d35e9882d6e6b222fdc35f0f31b7d10
Raymond K. Hessel <raymondkhessel2@gmail.com>
2012-03-06 02:21:52 -0800

    improve server.js to serve any file from the site folder's root

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-var webServer = require('express').createServer();
+//include libraries
+var path      = require('path'),
+    express   = require('express'),
+    
+//setup webserver object
+    webServer = express.createServer();
 webServer.listen(3005);
 
+
+/** forward assests requests to site/assets folder */
 webServer.get('/assets/*', function(request, response){
-    //forward assests requests to site/assets folder
     //request.params[0] is the requested filename
     response.sendfile(__dirname + '/site/assets/' + request.params[0]);
 });
 
-webServer.get('*', function(request, response){
+/** load the index automatically */
+webServer.get('/', function(request, response){
     response.sendfile(__dirname + '/site/index.html');
 });
+
+/** handle all other GET requests */
+webServer.get('/:filename.:format?', function(request, response){
+  var fmt = request.params.format || 'html', //default to html format
+      file = __dirname + '/site/' + request.params.filename +'.' + fmt;
+
+  //if the file exists, send it, otherwise 404
+  path.exists(file, function(exists){
+    if(exists){
+      response.sendfile(file);
+    } else {
+      response.send(404);
+    }
+  });
+});
+
+
